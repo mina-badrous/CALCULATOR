d34cd46c93ff2bfd09cc1cc0cd2ac413f2b4e346
unknown <minabadrous@yahoo.com>
2018-04-29 11:36:55 +0200

    empty project

---
+++ calculator/calculator/calculator.c
@@ -0,0 +1,17 @@
+/*
+ * calculator.c
+ *
+ * Created: 29/04/2018 11:34:44
+ *  Author: BADROUS
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
